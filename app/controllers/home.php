c33a9e666a79a0c67ccc554cf5eed0281ec8c2d0
juanmnl07 <juanmnl07@gmail.com>
2014-10-05 22:10:09 -0600

    first commit

---
+++ app/controllers/home.php
@@ -0,0 +1,15 @@
+<?php
+/**
+* 
+*/
+class Home extends Controller
+{
+	
+	public function index($name = '')
+	{
+		$user = $this->model('user');
+		$user->name = $name;
+		$this->view('home/index', ['name' => $user->name]);
+	}
+
+}
